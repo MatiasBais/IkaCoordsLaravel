74b16dd1990d3ebbc0233e90b6701d54eafc7a4d
Matias Bais <matiasbais1998@gmail.com>
2024-02-26 15:01:28 -0300

    ads

--- resources/views/layouts/searchBar.blade.php
+++ resources/views/layouts/searchBar.blade.php
@@ -10,4 +10,16 @@
         <option value="Alpha">Alpha</option>
         <option value="Kerberos">Kerberos</option>
     </select>
+    <script type="text/javascript">
+        atOptions = {
+            'key': 'dc2f6a89eef26f3321bc61ee25aa0b12',
+            'format': 'iframe',
+            'height': 250,
+            'width': 300,
+            'params': {}
+        };
+        document.write('<scr' +
+            'ipt type="text/javascript" src="//www.topcreativeformat.com/dc2f6a89eef26f3321bc61ee25aa0b12/invoke.js"></scr' +
+            'ipt>');
+    </script>
 </div>
